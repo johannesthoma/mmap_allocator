23b4fa9387f584113e052c1a8f23a1fb6d3183fb
Johannes Thoma <johannes.thoma@rise-world.com>
2012-09-21 13:23:26 +0200

    alloc/dealloc with fprintf works now.
    The rebind structure was required.

--- mmap_allocator.h
+++ mmap_allocator.h
@@ -11,14 +11,26 @@ public:
 		typedef T* pointer;
 		typedef const T* const_pointer;
 
-		pointer allocate(size_type n, std::allocator<void>::const_pointer hint=0)
+		template<typename _Tp1>
+	        struct rebind
+        	{ 
+			typedef mmap_allocator<_Tp1> other; 
+		};
+
+		pointer allocate(size_type n, const void *hint=0)
 		{
 			fprintf(stderr, "Alloc %d bytes.\n", n);
 			return std::allocator<T>::allocate(n, hint);
 		}
 
-		mmap_allocator() throw(): std::allocator<T>() { fprintf(stderr, "Hallo allokator!\n"); }
+		void deallocate(pointer p, size_type n)
+		{
+			fprintf(stderr, "Dealloc %d bytes (%p).\n", n, p);
+			return std::allocator<T>::deallocate(p, n);
+		}
+
+		mmap_allocator() throw(): std::allocator<T>() { fprintf(stderr, "Hello allocator!\n"); }
 		mmap_allocator(const mmap_allocator &a) throw(): std::allocator<T>(a) { }
-//		~mmap_allocator() throw(): ~std::allocator<T>() { }
+		~mmap_allocator() throw() { }
 	};
 }
--- test_allocator.cpp
+++ test_allocator.cpp
@@ -24,15 +24,22 @@ int main(int argc, char ** argv)
 {
 	generate_test_file();
 
-	// vector<int, mmap_allocator<int> > int_vec(1024, 0, mmap_allocator<int>());
+	vector<int, mmap_allocator<int> > int_vec(1024, 0, mmap_allocator<int>());
+	vector<int> test_vec;
+	vector<int> test_vec2;
+
+	test_vec = test_vec2;
+
 	mmap_allocator<int> a;
 	a.allocate(10);
 
-	vector<int, mmap_allocator<int> > *int_vec = new vector<int, mmap_allocator<int> >(1024, 0, mmap_allocator<int>());
+	vector<int, mmap_allocator<int> > *int_vec_pointer = new vector<int, mmap_allocator<int> >(1024, 0, mmap_allocator<int>());
+/*
 	int i;
 	for (i=0;i<1024;i++) {
-		assert((*int_vec)[i] == i);
+		assert((*int_vec_pointer)[i] == i);
 	}
+*/
 
 	return 0;
 }
